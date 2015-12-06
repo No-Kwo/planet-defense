cc8dce26a237fecc85e1b728ed268c11078c0886
no-paj <nopaj@openmailbox.org>
2015-12-06 04:25:20 +0100

    Fixed the ennemies behavior using angular coordinates

--- classes/Enemy.py
+++ classes/Enemy.py
@@ -1,50 +1,41 @@
+from math import sqrt, cos, sin, radians
+
 import pygame
 import random
 
+
 class Enemy(object):
-    '''
+    """
     A class that represents an ennemy.
     This class has to be inherited from.
-    '''
+    """
 
     max_hp = 10
     speed = 1.0
     damage = 1
     _spritePath = ''
-    posX = 50
-    posY = 50
 
     def __init__(self, screen_size, max_hp=max_hp):
+        self.screen_size = screen_size
         self.max_hp = max_hp
-        rand = random.randint(1,6); #very basic way to decide which asteriod to spawn
+        rand = random.randint(1, 6)  # very basic way to decide which asteriod to spawn
         self.damage = rand
-        self._spritePath = "assets/sprites/a{0}".format(rand)+".png"
-        self.sprite = pygame.image.load(self._spritePath).convert()
+        self._spritePath = "assets/sprites/a{0}".format(rand) + ".png"
+        self.sprite = pygame.image.load(self._spritePath).convert_alpha()
 
         # random spawn
-        r = random.randint(0,3) # 0 - left , 1 right 2 top 3 bottom
-        if r==0:
-            self.posX = 0
-            self.posY = random.randint(0,screen_size[1])
-        elif r==1:
-            self.posX = screen_size[0]
-            self.posY = random.randint(0,screen_size[1])
-        elif r==2:
-            self.posX = random.randint(0,screen_size[0])
-            self.posY = 0
-        elif r==3:
-            self.posX = random.randint(0,screen_size[0])
-            self.posY = screen_size[1]
+        self.radius = sqrt(self.screen_size[0] ** 2 + self.screen_size[1] ** 2) / 2.
+        print cos(radians(180))
+        # random angle
+        self.angle = random.randint(0, 360)
+        print(self.angle)
+        self.x = self.radius * cos(self.angle) + self.screen_size[0] / 2.
+        self.y = self.radius * sin(self.angle) + self.screen_size[1] / 2.
+        # print(self.x, self.y)
 
+    def update(self):
 
-    def update(self,target): #move towards the target
-        speed = self.speed
-        if(self.posX>target[0]):
-            self.posX-=speed;
-        else:
-            self.posX+=speed;
+        self.radius -= self.speed
 
-        if(self.posY>target[1]):
-            self.posY-=speed;
-        else:
-            self.posY+=speed;
+        self.y = self.radius * sin(self.angle) + self.screen_size[1] / 2.
+        self.x = self.radius * cos(self.angle) + self.screen_size[0] / 2.
--- game.py
+++ game.py
@@ -5,18 +5,20 @@ from classes.enemy import Enemy
 import sys
 import threading
 
+
 def set_interval(func, sec):
     def func_wrapper():
-        set_interval(func, sec) 
-        func()  
+        set_interval(func, sec)
+        func()
+
     t = threading.Timer(sec, func_wrapper)
     t.start()
     return t
 
-class PlanetDefense:
 
-    mousePos = (100,100) #current mouse position
-    screenSize = (960,600)
+class PlanetDefense:
+    mousePos = (100, 100)  # current mouse position
+    screenSize = (960, 600)
     ships = []
     enemies = []
 
@@ -28,34 +30,34 @@ class PlanetDefense:
         self.exit = False
         self.players = [Player()]
 
-    def update(self,e):
+    def update(self, e):
         for s in self.enemies:
-            s.update(self.screenSize)
-    #    for object in self.enemies,self.ships,self.players:
-         #   object.update()
+            s.update()
+            #    for object in self.enemies,self.ships,self.players:
+            #   object.update()
     
-    def draw_sprite(self,sprite,position):
-        self.display.blit(sprite,(position[0]-sprite.get_size()[0]/2,position[1]-sprite.get_size()[1]/2))
+    def draw_sprite(self, sprite, x, y):
+        self.display.blit(sprite, (x - sprite.get_size()[0] / 2, y - sprite.get_size()[0] / 2))
 
     def draw(self):
-        self.draw_sprite(self.players[0].sprite,(self.screenSize[0]/2,self.screenSize[1]/2))
+        self.draw_sprite(self.players[0].sprite, self.screenSize[0] / 2, self.screenSize[1] / 2)
 
-        for s in self.ships+self.enemies:
-            self.draw_sprite(s.sprite,(s.posX,s.posY))
+        for s in self.ships + self.enemies:
+            self.draw_sprite(s.sprite, s.x, s.y)
 
     def build_ship(self):
         print(pygame.mouse.get_pos())
         self.ships.append(Ship(pygame.mouse.get_pos()))
 
     def spawn_asteroid(self):
-        self.enemies.append(Enemy((self.screenSize[0]/2,self.screenSize[1]/2)))
+        self.enemies.append(Enemy(self.screenSize))
 
     def run(self):
         clock = pygame.time.Clock()
-        set_interval(self.spawn_asteroid,1)
+        set_interval(self.spawn_asteroid, 1)
         while not self.exit:
             clock.tick(60)
-            self.display.fill((0,0,0)) #clear the previous screen
+            self.display.fill((0, 0, 0))  # clear the previous screen
             for e in pygame.event.get():
                 if e.type == pygame.QUIT:
                     self.exit = True
@@ -63,14 +65,15 @@ class PlanetDefense:
                     self.mousePos = e.pos
                 elif e.type == pygame.MOUSEBUTTONDOWN:
                     self.build_ship()
-                print(e)
-            self.update(pygame.event) # update the positions etc
-            self.draw()               # draw the game screen
+                #print(e)
+            self.update(pygame.event)  # update the positions etc
+            self.draw()  # draw the game screen
 
             pygame.display.update()
 
         pygame.quit()
         sys.exit()
+
 
 game = PlanetDefense()
 game.run()
